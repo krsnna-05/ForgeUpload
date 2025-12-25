987ad8904a5e1578df82c3b8999de6fd69e6e984
devkrsna <krishnagavali973@gmail.com>
2025-12-25 22:50:50 +0530

    Add configuration files and enhance styling with Tailwind CSS and utility functions

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { Topbar } from "./components";
 
 const App = () => {
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className=" min-h-screen bg-background text-foreground">
       <Topbar />
 
       <main className="max-w-7xl mx-auto px-6 py-12">
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,23 @@
 import { defineConfig } from "vite";
+import path from "path";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
